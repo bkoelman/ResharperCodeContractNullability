2b3131843ae8ab2122ae7b37afe6d9e3eadd5f32
bkoelman <bartkoelman@gmail.com>
2015-09-21 00:37:16 +0200

    Metadata updates for next release: v1.0.2

--- src/CodeContractNullability/CodeContractNullability/Properties/AssemblyInfo.cs
+++ src/CodeContractNullability/CodeContractNullability/Properties/AssemblyInfo.cs
@@ -33,9 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-// =====
-// NOTE: Also update in CodeContractNullability.nuspec and source.extension.vsixmanifest
-// =====
-
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
