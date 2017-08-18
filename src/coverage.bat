1c34e73ded8c2f2bf78ec45c55e01e9af47e5c6e
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-08-19 01:07:31 +0200

    Package updates and code coverage fix

--- src/coverage.bat
+++ src/coverage.bat
@@ -1,5 +1,5 @@
 @echo off
 rd /s /q coverage 2>nul
 md coverage
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:".\packages\xunit.runner.console.2.2.0\tools\xunit.console.x86.exe" -targetargs:".\CodeContractNullability\CodeContractNullability.Test\bin\Debug\CodeContractNullability.Test.dll -noshadow" -filter:"+[CodeContractNullability]*  -[CodeContractNullability]CodeContractNullability.Properties.*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CodeContractNullabilityCoverage.xml
-.\packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe -targetdir:.\coverage -reports:.\coverage\CodeContractNullabilityCoverage.xml
+"%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" -register:user -target:"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe" -targetargs:".\CodeContractNullability\CodeContractNullability.Test\bin\Debug\CodeContractNullability.Test.dll -noshadow" -filter:"+[CodeContractNullability]*  -[CodeContractNullability]CodeContractNullability.Properties.*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CodeContractNullabilityCoverage.xml
+"%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.10\tools\ReportGenerator.exe" -targetdir:.\coverage -reports:.\coverage\CodeContractNullabilityCoverage.xml
