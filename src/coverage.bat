cc9547080f7d377dd01bcdfeffefbc8ef2f10e7d
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-08-20 04:53:27 +0200

    Updated to SDK-based project file format

--- src/coverage.bat
+++ src/coverage.bat
@@ -1,5 +1,13 @@
 @echo off
 rd /s /q coverage 2>nul
 md coverage
-"%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" -register:user -target:"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe" -targetargs:".\CodeContractNullability\CodeContractNullability.Test\bin\Debug\CodeContractNullability.Test.dll -noshadow" -filter:"+[CodeContractNullability]*  -[CodeContractNullability]CodeContractNullability.Properties.*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CodeContractNullabilityCoverage.xml
-"%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.10\tools\ReportGenerator.exe" -targetdir:.\coverage -reports:.\coverage\CodeContractNullabilityCoverage.xml
+
+set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
+set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.10\tools\ReportGenerator.exe"
+set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe"
+set target=".\CodeContractNullability\CodeContractNullability.Test\bin\Debug\net45\CodeContractNullability.Test.dll -noshadow"
+set coveragefile=".\coverage\CodeContractNullabilityCoverage.xml"
+
+%opencover% -register:user -target:%testrunner% -targetargs:%target% -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:%coveragefile%
+%reportgenerator% -targetdir:.\coverage -reports:%coveragefile%
+
