0fa9bb352732a5ee1cbaad42b4084fc287850f41
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-05-03 10:28:19 +0200

    Added tests for local functions

---
+++ src/CodeContractNullability/CodeContractNullability.Test/Specs/LocalFunctionSpecs.cs
@@ -0,0 +1,55 @@
+﻿using CodeContractNullability.Test.TestDataBuilders;
+using Xunit;
+
+namespace CodeContractNullability.Test.Specs
+{
+    /// <summary>
+    /// Tests for reporting nullability diagnostics on local functions.
+    /// </summary>
+    public sealed class LocalFunctionSpecs : NullabilityTest
+    {
+        [Fact]
+        public void When_local_function_parameter_is_nullable_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    public void M()
+                    {
+                        void L(string s)
+                        {
+                            throw new NotImplementedException();
+                        }
+
+                        L(null);
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyNullabilityDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_local_function_return_value_is_nullable_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    public void M()
+                    {
+                        string L()
+                        {
+                            throw new NotImplementedException();
+                        }
+
+                        string s = L();
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyNullabilityDiagnostic(source);
+        }
+    }
+}
