da8ef85b576e84d040d3ec0f702bfca4c203f756
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-05-02 21:38:25 +0200

    Added initial test

--- src/CodeContractNullability/CodeContractNullability.Test/Specs/ScanningForExternalAnnotationsSpecs.cs
+++ src/CodeContractNullability/CodeContractNullability.Test/Specs/ScanningForExternalAnnotationsSpecs.cs
@@ -1,4 +1,7 @@
-﻿using CodeContractNullability.ExternalAnnotations;
+﻿using System;
+using System.IO;
+using CodeContractNullability.ExternalAnnotations;
+using FluentAssertions;
 using TestableFileSystem.Fakes.Builders;
 using TestableFileSystem.Interfaces;
 using Xunit;
@@ -7,14 +10,35 @@ namespace CodeContractNullability.Test.Specs
 {
     public sealed class ScanningForExternalAnnotationsSpecs
     {
-        [Fact(Skip = "TODO: Implement tests")]
-        public void XXX()
+        [Fact]
+        public void When_scanning_for_external_annotations_it_must_succeed()
         {
-            IFileSystem fileSystem = new FakeFileSystemBuilder().Build();
+            // Arrange
+            string localAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string filePath = Path.Combine(localAppDataFolder,
+                @"JetBrains\Installations\ReSharperPlatformVs15_57882815\ExternalAnnotations\.NETFramework\mscorlib\Annotations.xml");
+
+            IFileSystem fileSystem = new FakeFileSystemBuilder()
+                .IncludingTextFile(filePath,
+                    @"<?xml version=""1.0"" encoding=""utf-8""?>
+                    <assembly name=""mscorlib"">
+                      <member name=""M:System.String.Format(System.String,System.Object)"">
+                        <attribute ctor=""M:JetBrains.Annotations.NotNullAttribute.#ctor""/>
+                        <parameter name=""format"">
+                          <attribute ctor=""M:JetBrains.Annotations.NotNullAttribute.#ctor""/>
+                        </parameter>
+                      </member>
+                    </assembly>
+                ")
+                .Build();
 
             var resolver = new CachingExternalAnnotationsResolver(fileSystem, new LocalAnnotationCacheProvider(fileSystem));
 
-            resolver.EnsureScanned();
+            // Act
+            Action action = () => resolver.EnsureScanned();
+
+            // Assert
+            action.Should().NotThrow();
         }
     }
 }
