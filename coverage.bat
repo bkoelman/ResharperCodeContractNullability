2d905c535c06f8154b2921f5ca4840de51185c21
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-05-05 18:11:58 +0200

    Downgraded opencover package
    Due to bug in v4.7.922 described at https://github.com/OpenCover/opencover/issues/881

--- coverage.bat
+++ coverage.bat
@@ -3,7 +3,7 @@ rd /s /q coverage 2>nul
 md coverage
 
 set configuration=Debug
-set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe"
+set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
 set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\4.1.4\tools\net47\ReportGenerator.exe"
 set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.4.1\tools\net472\xunit.console.x86.exe"
 set target=".\src\CodeContractNullability\CodeContractNullability.Test\bin\%configuration%\net472\CodeContractNullability.Test.dll -noshadow"
