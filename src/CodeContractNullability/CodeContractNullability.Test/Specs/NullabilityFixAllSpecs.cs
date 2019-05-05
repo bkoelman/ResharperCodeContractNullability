57f1c0422260f7f4552b4db39fed40a5a19b7b8f
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2019-05-05 18:40:19 +0200

    Added specs for FixAll providers

---
+++ src/CodeContractNullability/CodeContractNullability.Test/Specs/NullabilityFixAllSpecs.cs
@@ -0,0 +1,27 @@
+﻿using CodeContractNullability.Test.TestDataBuilders;
+using Xunit;
+
+namespace CodeContractNullability.Test.Specs
+{
+    public sealed class NullabilityFixAllSpecs : NullabilityTest
+    {
+        [Fact]
+        public void When_parameter_types_are_reference_it_must_all_be_reported_and_fixed()
+        {
+            ParsedSourceCode source = new TypeSourceCodeBuilder()
+                .InGlobalScope(@"
+                    class C
+                    {
+                        void M([+NullabilityAttributePlaceholder+] string [|p1|], [+NullabilityAttributePlaceholder+] string [|p2|])
+                        {
+                        }
+                    }
+                ")
+                .Build();
+
+            VerifyNullabilityFixes(source,
+                CreateMessageForParameter("p1"),
+                CreateMessageForParameter("p2"));
+        }
+    }
+}
