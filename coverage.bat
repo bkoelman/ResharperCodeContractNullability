762d4c805fc53b68c24e73786ca2317396f51958
bart-degreed <bart@degreed.com>
2021-03-10 22:40:14 +0100

    Package updates

--- coverage.bat
+++ coverage.bat
@@ -3,8 +3,8 @@ rd /s /q coverage 2>nul
 md coverage
 
 set configuration=Debug
-set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
-set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\4.3.9\tools\net47\ReportGenerator.exe"
+set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.7.922\tools\OpenCover.Console.exe"
+set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\4.8.7\tools\net47\ReportGenerator.exe"
 set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.4.1\tools\net472\xunit.console.x86.exe"
 set target=".\src\CodeContractNullability\CodeContractNullability.Test\bin\%configuration%\net472\CodeContractNullability.Test.dll -noshadow"
 set filter="+[CodeContractNullability*]*  -[CodeContractNullability.Test*]*"
@@ -12,4 +12,3 @@ set coveragefile=".\coverage\CodeCoverage.xml"
 
 %opencover% -register:user -target:%testrunner% -targetargs:%target% -filter:%filter% -hideskipped:All -output:%coveragefile%
 %reportgenerator% -targetdir:.\coverage -reports:%coveragefile%
-
